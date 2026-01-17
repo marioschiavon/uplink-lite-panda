630256c655ca757d6b73ad2ad6525f2c3b9dfa41
Mario <82286821+marioschiavon@users.noreply.github.com>
2026-01-17 16:14:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   preview: {
     host: "0.0.0.0",
     port: 3000,
+    strictPort: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
